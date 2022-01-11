4057f471657319c6ab1a3e29c0b77a46b37342f5
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-11 21:11:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Form from "./FormComp";
+import FormFunction from "./FormFunction";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>l4_t4, задание 3: форма</h1>
+      <div className="forms">
+        <Form />
+        <FormFunction />
+      </div>
     </div>
   );
 }
